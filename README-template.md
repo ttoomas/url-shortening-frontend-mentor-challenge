651739acd118cf7a2ab9814a1eecab411094c07b
Tomas <folprecht.to@gmail.com>
2021-12-27 18:23:17 +0100

    Project upload

---
+++ README-template.md
@@ -0,0 +1,57 @@
+# Frontend Mentor - Shortly URL shortening API Challenge solution
+
+This is a solution to the [Shortly URL shortening API Challenge challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- Shorten any valid URL
+- See a list of their shortened links, even after refreshing the browser
+- Copy the shortened link to their clipboard in a single click
+- Receive an error message when the `form` is submitted if:
+  - The `input` field is empty
+
+### Links
+
+- Solution URL: [Github](https://github.com/ttoomas/url-shortening-frontend-mentor-challenge)
+- Live Site URL: [Live Site - Github](https://ttoomas.github.io/url-shortening-frontend-mentor-challenge/)
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+
+This is my best frontend-mentor challenge. Because I haven't had experience with API or local-storage yet. Thanks to this challenge, I learned to work with both the API and the local-storage. I practiced my skills in building a bigger project. I am proud of my solution.
+
+## Author
+
+- Twitter - [ttoomas_](https://twitter.com/ttoomas_)
+- Github - [ttoomas](https://github.com/ttoomas)
+- Frontend Mentor - [@ttoomas](https://www.frontendmentor.io/profile/ttoomas)
+
+## Acknowledgments
+
+I'd like to thank [Nikita Skrebets](https://stackoverflow.com/users/4873616/nikita-skrebets)  for his help when I'm stuck on the copy buttons.
---
+++ main.js
@@ -0,0 +1,129 @@
+// HEADER - HAMBURGER ANIMATION
+
+const header__toggle = document.querySelector(".header__toggle");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const fadeElems = document.querySelectorAll(".has-fade")
+
+header__toggle.addEventListener("click", () =>{
+	if (header.classList.contains("open")){
+		header.classList.remove("open");
+		body.classList.remove("noscroll");
+		header__toggle.classList.remove("open");
+		fadeElems.forEach(function(element){
+			element.classList.remove('fade-in');
+			element.classList.add('fade-out');
+		});
+	}
+	else{
+		header.classList.add("open");
+		body.classList.add("noscroll");
+		header__toggle.classList.add("open");
+		fadeElems.forEach(function(element){
+			element.classList.remove('fade-out');
+			element.classList.add('fade-in');
+		});
+	}
+});
+
+
+// LINKS
+const inputField = document.querySelector(".short__input");
+var button = document.querySelector(".short__button");
+
+const linkContainer = document.querySelector(".link__container");
+
+
+inputField.addEventListener("keyup", dataLoad);
+button.addEventListener("click", dataLoad);
+
+function dataLoad(e){
+	if(e&&e.keyCode === 13){
+		// console.log('entered');
+		requestData(inputField.value);
+	}
+	else if(e.type=="click"){
+		// console.log('clicked');
+		requestData(inputField.value);
+	}
+}
+
+function requestData(){
+	const linkInput = inputField.value;
+	// console.log(linkInput);
+
+	fetch(`https://api.shrtco.de/v2/shorten?url=${linkInput}`)
+	.then(res => res.json())
+	.then(data => linkInfo(data))
+}
+
+function linkInfo(data){
+	console.log(data);
+	inputField.value = "";
+	const shortPart = document.querySelector('.short__part');
+
+	if(data.error_code == "1"){
+		// console.log('error number 1 - no URL parameter');
+		
+		shortPart.classList.add('no-link-error');
+		shortPart.classList.remove('no-valid-error');
+	}
+	else if(data.error_code == "2"){
+		// console.log('error number 2 - not valid URL link')
+		
+		shortPart.classList.remove('no-link-error');
+		shortPart.classList.add('no-valid-error');
+	}
+	else{
+		// console.log("successful shortening of the URL link");
+		shortPart.classList.remove('no-link-error');
+		shortPart.classList.remove('no-valid-error');
+
+			const shortLink = data.result.short_link;
+			const originalLink = data.result.original_link;
+			const idShortLink = shortLink.substring(shortLink.lastIndexOf('/') + 1);
+
+			var apiDataLinks = `
+				<div class="link__item">
+					<p class="link__item__original">${originalLink}</p>
+					<div class="link__bx">
+						<a href="https://${shortLink}" id="${idShortLink}" class="link__item__short">${shortLink}</a>
+						<button id="button${idShortLink}" onclick="copyButton('${idShortLink}')" class="link__item__button copy">Copy</button>
+					</div>
+				</div>
+			`;
+			const apiShortLink = `${data.result.short_link}`
+
+			linkContainer.insertAdjacentHTML('afterbegin', apiDataLinks);
+
+			// Add margin to first shorted link
+			linkContainer.style.margin="1.5rem 0 0 0";
+
+			// Local Storage load data
+			const key = apiDataLinks;
+			localStorage.setItem(originalLink, key);
+	}
+}
+
+// Local Storage get items
+for (let i = 0; i < localStorage.length; i++){
+	const key = localStorage.key(i);
+	const value = localStorage.getItem(key);
+	// console.log(value);
+	linkContainer.insertAdjacentHTML('afterbegin', value);
+
+	// Add margin to first shorted link
+	linkContainer.style.margin="1.5rem 0 0 0";
+}
+
+
+// COPY TO CLIPBOARD
+function copyButton(id){
+	const shortLink = document.querySelector('#' + id);
+	navigator.clipboard.writeText(shortLink.innerText);
+	// Add and remove class for clicked button
+	const button = document.querySelector("#button" + id);
+	button.innerHTML = `Copied!`
+	button.classList.add('copied');
+	button.classList.remove('copy');
+}
